d4ee57fc7e3cf6687d7f78d345add15866103a05
mederel <mederel@gmail.com>
2024-12-20 18:24:10 +0100

    working example

--- src/main/java/fr/leyrdhin/utilities/Main.java
+++ src/main/java/fr/leyrdhin/utilities/Main.java
@@ -2,6 +2,9 @@ package fr.leyrdhin.utilities;
 
 import com.drew.imaging.ImageMetadataReader;
 import com.drew.metadata.Metadata;
+import com.drew.metadata.Tag;
+import com.drew.metadata.exif.ExifDirectoryBase;
+import com.drew.metadata.exif.ExifSubIFDDirectory;
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOCase;
 import org.apache.commons.io.filefilter.FalseFileFilter;
@@ -10,15 +13,30 @@ import org.apache.commons.io.filefilter.SuffixFileFilter;
 import org.apache.commons.io.filefilter.TrueFileFilter;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.text.SimpleDateFormat;
+import java.util.Collection;
+import java.util.Date;
 
 public class Main {
+    private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("yyyyMMdd_HHmmss");
+
     public static void main(String[] args) throws Exception {
         File rootDir = new File(args[0]);
-        for (File jpegFile : FileUtils.listFiles(rootDir, new SuffixFileFilter(new String[]{".jpg", ".jpeg"}, IOCase.INSENSITIVE), FalseFileFilter.INSTANCE)) {
+        for (File jpegFile : FileUtils.listFiles(rootDir, new SuffixFileFilter(new String[]{".jpg", ".jpeg"}, IOCase.INSENSITIVE), TrueFileFilter.INSTANCE)) {
             System.out.println(jpegFile.getAbsolutePath());
             Metadata metadata = ImageMetadataReader.readMetadata(jpegFile);
-            System.out.println(metadata);
+            Collection<ExifSubIFDDirectory> exifDirectoryBase = metadata.getDirectoriesOfType(ExifSubIFDDirectory.class);
+            for (ExifDirectoryBase directory : exifDirectoryBase) {
+                Date date = directory.getDate(ExifDirectoryBase.TAG_DATETIME_DIGITIZED);
+                if (date != null) {
+                    String formattedDate = DATE_FORMAT.format(date);
 
+                    File newFile = new File(jpegFile.getParent(), formattedDate + ".jpg");
+                    FileUtils.moveFile(jpegFile, newFile);
+                    break;
+                }
+            }
         }
     }
 }
---
+++ src/test/java/fr/leyrdhin/utilities/MainTest.java
@@ -0,0 +1,47 @@
+package fr.leyrdhin.utilities;
+
+
+import org.apache.commons.io.FileUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
+
+import java.nio.file.CopyOption;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.everyItem;
+import static org.hamcrest.Matchers.matchesRegex;
+
+class MainTest {
+
+    @TempDir
+    Path temporaryFolder;
+
+    Path imagesDir;
+
+    @BeforeEach
+    void setUp() throws Exception {
+        Path srcDir = Path.of("src", "test", "resources", "images");
+        imagesDir = temporaryFolder.resolve("images");
+        FileUtils.copyDirectory(srcDir.toFile(), imagesDir.toFile());
+    }
+
+    @Test
+    void testMain() throws Exception {
+        try (Stream<Path> files = Files.list(imagesDir)) {
+            System.out.println(files.toList());
+        }
+
+        Main.main(new String[]{imagesDir.toString()});
+
+        try (Stream<Path> files = Files.list(imagesDir)) {
+            List<String> fileNames = files.map(Path::getFileName).map(Path::toString).toList();
+
+            assertThat(fileNames, everyItem(matchesRegex("\\d{8}_\\d{6}\\.(jpg|JPEG|JPG|jpeg)")));
+        }
+    }
+}
